16edfc812e22a1dcc10c2077ffe3736fa08f6d02
Saki Fu <sakifu.86@gmail.com>
2015-11-02 03:07:53 -0800

    First commit

---
+++ config.py
@@ -0,0 +1,7 @@
+#Please put the values here.
+key="R74El6Yd5te5dQeRr4AFhyq95"
+secret="nj3rAJfIER0TyrteelXLJs9nF9gjmSlDS1bPFfd7tGH5vGllX5"
+access_key="3067917175-q30lpWfbFG0kbyWTSXFNV7jI5yPLLBIjcZwmZiD"
+access_secret="PuvdP8TMC9ASjIUMs86ikCdq445eyR7chcMRP19Jzd7Ll"
+
+
---
+++ twitter_functions.py
@@ -0,0 +1,60 @@
+import twitter
+import util
+from config import *
+
+BOSTON_WOEID = 2367105
+api = twitter.Api(consumer_key=key,consumer_secret=secret,access_token_key=access_key,access_token_secret=access_secret)
+
+def search(searchTerm):
+    """
+    Print recent tweets containing `searchTerm`.
+
+    To test this function, at the command line run:
+        python twitter_api.py --search=<search term>
+    For example,
+        python twitter_api.py --search=python
+    """
+    api = twitter.Api(consumer_key=key,consumer_secret=secret,access_token_key=access_key,access_token_secret=access_secret)
+    tweets = api.GetSearch(searchTerm)
+    for tweet in tweets:
+        util.safe_print(tweet.GetText())
+
+def trendingTopics():
+    """
+    Print the currently trending topics.
+
+    To test this function, at the command line run:
+        python twitter_api.py -t
+    """
+    api = twitter.Api(consumer_key=key,consumer_secret=secret,access_token_key=access_key,access_token_secret=access_secret)
+    trending_topics = api.GetTrendsWoeid(BOSTON_WOEID)
+    for topic in trending_topics:
+        util.safe_print(topic.name)
+
+def userTweets(username):
+    """
+    Print recent tweets by `username`.
+
+    You may find the twitter.Api() function GetUserTimeline() helpful.
+
+    To test this function, at the command line run:
+        python twitter_api.py -u <username>
+    For example,
+        python twitter_api.py -u bostonpython
+    """
+    api = twitter.Api(consumer_key=key,consumer_secret=secret,access_token_key=access_key,access_token_secret=access_secret)
+    user_tweet = api.GetUserTimeline(screen_name=username)
+    for tweet in user_tweet:
+        util.safe_print(tweet.GetText())
+
+def trendingTweets():
+    """
+    Print tweets for all the trending topics.
+
+    To test this function, at the command line run:
+        python twitter_api.py -w
+    """
+    api = twitter.Api(consumer_key=key,consumer_secret=secret,access_token_key=access_key,access_token_secret=access_secret)
+    trending_topics = api.GetTrendsWoeid(BOSTON_WOEID)
+    for tweet in trending_topics:
+        util.safe_print(tweet.GetText())
